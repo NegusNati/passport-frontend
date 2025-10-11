319d5ce0ff400973faeec956584339113d182330
negusnati <natnaelbirhanu22@gmail.com>
2025-10-11 23:28:12 +0300

    fix: update dependencies in pnpm-lock.yaml and adjust Vite configuration: Upgraded various packages including React and Rollup to their latest versions for improved performance and compatibility. Modified Vite configuration to use the correct import for defining the config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
